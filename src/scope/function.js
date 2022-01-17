26e28ca83fc2ad29c0eea7091af4881597cebac3
pikyr <pikytdv@gmail.com>
2022-01-16 21:47:30 -0300

    Fin de clase 4 - Function scope

---
+++ src/scope/function.js
@@ -0,0 +1,20 @@
+const fruits = () => {
+  const fruit = "Apple";
+
+  console.log(fruit);
+}
+
+fruits();
+
+const anotherFunction = () => {
+  var x = 1;
+  var x = 2;
+
+  let y = 3;
+  // let y = 4;
+
+  console.log(x);
+  console.log(y);
+}
+
+anotherFunction();
